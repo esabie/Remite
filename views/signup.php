06aae62cb1430cf3f29a39d6d1bfbb2eaafcbf80
EUGENE OSEI SABIE <sabieeugeneosei@yahoo.com>
2024-07-08 19:32:39 +0000

    sign up set up

--- views/signup.php
+++ views/signup.php
@@ -23,22 +23,24 @@
 
     <!-- Showcase -->
     <section
-      class="signup ">
+      class="signup  ">
+      <div class="container mx-auto text-center  z-10"></div>
       <div class="container mx-auto text-center z-10"></div>
-      <div class="container mx-auto text-center z-10"></div>
-        <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
+        <div class="bg-white p-8 rounded-lg shadow-md md:w-350 mx-auto my-3">
             <form id="convertForm" method="GET" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
                 <div class="form-group">
-                    <label for="sendAmount">Currency Pair:</label>
-                    <input type="text" id="currencyPair" name="sendAmount" class="text-black" readonly value="<?php echo htmlspecialchars('GBP / GHS'); ?>">
+                <div class="logo" ></div>
+                <h6 class="text-center">Login to your account</h6>
+                <h1 class="text-center text-muted smalls">Don't have an account?
+                    <a href="home.php"> Here</a>
+                </h1>
+                    <input type="text" name="email" class="text-black" placeholder="Email Address" required>
                 </div>
                 <div class="form-group">
-                    <label for="sendAmount">Send Amount:</label>
-                    <input type="text" id="sendAmount" name="sendAmount" class="text-black" required value="<?php echo htmlspecialchars($sendAmount ?? ''); ?>">
-                </div>
-                <div class="form-group">
-                    <label for="equivalentAmount">Equivalent Amount:</label>
-                    <input type="text" id="equivalentAmount" name="equivalentAmount" readonly value="<?php echo htmlspecialchars($equivalent); ?>">
+                    <!-- <label for="sendAmount">Send Amount:</label>
+                    <input type="text" id="sendAmount" name="sendAmount" class="text-black" required value="<?php echo htmlspecialchars($sendAmount ?? ''); ?>"> -->
+                    <input type="text" name="psw" class="text-black" placeholder="Password" required>
+
                 </div>
                 <button type="submit" name="convert">Convert</button>
             </form>
