9c0dc57c4930ae282822cdf33e9cb2d350eb17fa
EUGENE OSEI SABIE <sabieeugeneosei@yahoo.com>
2024-07-13 23:55:21 +0000

    transaction page

--- public/views/transaction.php
+++ public/views/transaction.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/styles.css" />
     <title>eRem - Send Money Anywhere</title>
@@ -23,24 +24,13 @@
 
   <section
       class="transaction text-black ">
+      <div class="row">
       <div class="container mx-auto text-center z-10"></div>
         <div class="bg-white p-8 rounded-lg shadow-md md:w-800 mx-auto my-3">
             <form id="convertForm" method="GET" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
                 <div class="container">
                     <h1 class="form-title text-center ">TRANSACTION<h1>
                         <form action="#">
-                            <div class="form-group text-black">
-                                <label for = "firstName"> Sender First Name</label>
-                                <input type="text" id="senderfirstName" name="secondfirstName" required>
-                            </div>
-                            <div class="form-group text-black">
-                                <label for = "firstName"> Sender Last Name</label>
-                                <input type="text" id="senderlastName" name="secondlastName" required>
-                            </div>
-                            <div class="form-group text-black">
-                                <label for = "firstName"> First Name</label>
-                                <input type="text" id="firstName" name="firstName" readonly>
-                            </div>
                             <div class="form-group text-black">
                                 <label for = "firstName">Last Name</label>
                                 <input type="text" id="secondName" name="secondName">
@@ -64,20 +54,46 @@
 
                             <div class="row">
                                 <div class="form-group col-md-6 relative">
-                                    <label for = "firstName"> Network</label>
-                                    <input type="text" id="firstName" name="firstName">
+                                    <label for = "firstName"> Sender First Name</label>
+                                    <input type="text" id="senderfirstName" name="senderfirstName" required>
+                                </div>
+                                <div class="form-group col-md-6 relative">
+                                    <label for = "lastName"> Sender Last Name</label>
+                                    <input type="text" id="senderlastName" name="senderlastName" required>
+                                </div>
+                                <div class="form-group col-md-6 relative">
+                                    <label for = "firstName"> Beneficiary Phone Number</label>
+                                    <input type="phone" id="benef_phone" name="phone" required>
+                                </div>
+                                <div class="form-group col-md-6 relative">
+                                    <label for = "mno"> Network</label>
+                                    <select name="mno" id="mno">
+                                        <option value="zeepay">Zeepay</option>
+                                        <option value="mtn">MTN</option>
+                                        <option value="vodafone">Vodafone</option>
+                                        <option value="airteltigo">AirtelTigo</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-12 relative">
+                                    <label for = "firstName"> Sender First Name</label>
+                                    <input type="text" id="senderfirstName" name="secondfirstName" required>
+                                </div>
+                                <div class="form-group col-md-6 relative">
+                                    <label for = "firstName"> Amount</label>
+                                    <input type="text" id="amount" name="amount" required>
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for = "firstName"> Colombia</label>
                                     <input type="text" id="firstName" name="firstName">
                                 </div>
-                            </div>
+                            
                         </form>
 
                 </div>
-
+                </div>
                 <button type="submit" class="submit-button">Send Money</button>
             </form>
+            </div>
         </div>
     </section>
 
